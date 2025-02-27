e90ccfbd97e2e80307bc1a8a719a476d79a9910e
hugo kermabon <hugo.kermabonbobinnec@concordia.ca>
2025-02-27 16:08:05 -0500

    card writer loop

--- create-card.py
+++ create-card.py
@@ -3,6 +3,7 @@ from smartcard.util import toHexString
 from smartcard.CardConnection import CardConnection
 import time
 import random
+import smartcard
 
 
 KEY_A = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
@@ -45,6 +46,7 @@ def authenticate_with_keyA(connection, sector):
         return True
     else:
         print(f"Authentication failed. SW1: {sw1}, SW2: {sw2}")
+        exit(0)
         return False
 
 
@@ -99,95 +101,131 @@ def read_uid(connection):
         print(f"Failed to read UID. SW1: {sw1}, SW2: {sw2}")
         return False
 
+def wait_for_card():
+    """Waits for a card to be inserted and returns the connection object."""
+    r = readers()
+    if not r:
+        raise Exception("No smart card readers found.")
+
+    reader = r[0]  # Use the first available reader
+    connection = reader.createConnection()
+
+    while True:
+        time.sleep(0.5)
+        print("waiting for card")
+        try:
+            connection.connect()  # Try to connect
+            return connection  # Return the connection object
+        except smartcard.Exceptions.NoCardException:
+            pass  # Keep waiting until a card is inserted
+        except Exception:
+            # traceback.print_exc()
+            # time.sleep(5)
+            reset()
+
 
 def main():
-    # Get the first available reader
-    reader = get_reader()
-    connection = reader.createConnection()
-    connection.connect()
 
-    uid = read_uid(connection)
-    if uid:
-        print(f"UID: {toHexString(uid)}")
+    while True:
 
-    load_keyA(connection)
+        try:
+            connection = wait_for_card()
 
-    #### FLAG 1 JUST ENCODED IN THE CARD, SECTOR 1
-    sector = 1
+            # # Get the first available reader
+            # reader = get_reader()
+            # connection = reader.createConnection()
+            # connection.connect()
 
-    # FLAG1_1 in block 4
-    block = 4
-    if write_block(connection, sector, block, FLAG1_1):
-        print("Data written successfully!")
+            uid = read_uid(connection)
+            if uid:
+                print(f"UID: {toHexString(uid)}")
 
-    # FLAG1_2 in block 5
-    block = 5
-    if write_block(connection, sector, block, FLAG1_2):
-        print("Data written successfully!")
+            load_keyA(connection)
 
-    # FLAG1_3 in block 6
-    block = 6
-    if write_block(connection, sector, block, FLAG1_3):
-        print("Data written successfully!")
-        
+            #### FLAG 1 JUST ENCODED IN THE CARD, SECTOR 1
+            sector = 1
 
-    # # EXTRAFLAG?
-    # sector = 3
-    # block = 12 # and 13 and 14
-    # if write_block(connection, sector, block, EXTRAFLAG_1):
-    #     print("Data written successfully!")
+            # FLAG1_1 in block 4
+            block = 4
+            if write_block(connection, sector, block, FLAG1_1):
+                print("Data written successfully!")
 
+            # FLAG1_2 in block 5
+            block = 5
+            if write_block(connection, sector, block, FLAG1_2):
+                print("Data written successfully!")
 
+            # FLAG1_3 in block 6
+            block = 6
+            if write_block(connection, sector, block, FLAG1_3):
+                print("Data written successfully!")
+                
 
-
-    #### DATA FOR FLAG 3 AT SECTOR 10
-    sector = 10
-
-    # FIRSTNAME:NED
-    block = 40
-    first_name  = [0x46,0x49,0x52,0x53, 0x54,0x4e,0x41,0x4d, 0x45,0x3a,0x4e,0x45, 0x44,0x00,0x00,0x00]
-    if write_block(connection, sector, block, first_name):
-        print("Data written successfully!")
-
-    # LASTNAME:???
-    block = 41
-    last_name   = [0x4c,0x41,0x53,0x54, 0x4e,0x41,0x4d,0x45, 0x3a,0x3f,0x3f,0x3f, 0x00,0x00,0x00,0x00]
-    if write_block(connection, sector, block, last_name):
-        print("Data written successfully!")
-
-    # BALANCE:0015$80c
-    block = 42
-    balance     = [0x42,0x41,0x4c,0x41, 0x4e,0x43,0x45,0x3a, 0x30,0x30,0x31,0x35, 0x24,0x38,0x30,0x63]
-    if write_block(connection, sector, block, balance):
-        print("Data written successfully!")
+            # # EXTRAFLAG?
+            # sector = 3
+            # block = 12 # and 13 and 14
+            # if write_block(connection, sector, block, EXTRAFLAG_1):
+            #     print("Data written successfully!")
 
 
-    #### CONTINUE DATA FOR FLAG 3 AT SECTOR 11
-    sector = 11
 
-    # POSTCODE:H3G1M8
-    block = 44
-    postal_code     = [0x50,0x4f,0x53,0x54, 0x43,0x4f,0x44,0x45, 0x3a,0x48,0x33,0x47, 0x31,0x4d,0x38,0x00]
-    if write_block(connection, sector, block, postal_code):
-        print("Data written successfully!")
 
-    # PINCODE:XXXX
-    block = 45
-    random.seed(time.time_ns())
-    pin_code = ""
-    for i in range(4):
-        pin_code += str(random.randrange(10))
-    print("Pin Code: ", pin_code)
-    pin_code_data   = [0x50,0x49,0x4e,0x43, 0x4f,0x44,0x45,0x3a] + list(bytearray(pin_code, 'utf-8')) + [0x00,0x00,0x00,0x00]
-    print("Pin data: ", pin_code_data)
-    if write_block(connection, sector, block, pin_code_data):
-        print("Data written successfully!")
+            #### DATA FOR FLAG 3 AT SECTOR 10
+            sector = 10
 
-    # BIRTH:30/06/1998
-    block = 46
-    birthdate     = [0x42,0x49,0x52,0x54,0x48,0x3a,0x33,0x30,0x2f,0x30,0x36,0x2f,0x31,0x39,0x39,0x38]
-    if write_block(connection, sector, block, birthdate):
-        print("Data written successfully!")
+            # FIRSTNAME:NED
+            block = 40
+            first_name  = [0x46,0x49,0x52,0x53, 0x54,0x4e,0x41,0x4d, 0x45,0x3a,0x4e,0x45, 0x44,0x00,0x00,0x00]
+            if write_block(connection, sector, block, first_name):
+                print("Data written successfully!")
+
+            # LASTNAME:???
+            block = 41
+            last_name   = [0x4c,0x41,0x53,0x54, 0x4e,0x41,0x4d,0x45, 0x3a,0x3f,0x3f,0x3f, 0x00,0x00,0x00,0x00]
+            if write_block(connection, sector, block, last_name):
+                print("Data written successfully!")
+
+            # BALANCE:0015$80c
+            block = 42
+            balance     = [0x42,0x41,0x4c,0x41, 0x4e,0x43,0x45,0x3a, 0x30,0x30,0x31,0x35, 0x24,0x38,0x30,0x63]
+            if write_block(connection, sector, block, balance):
+                print("Data written successfully!")
+
+
+            #### CONTINUE DATA FOR FLAG 3 AT SECTOR 11
+            sector = 11
+
+            # POSTCODE:H3G1M8
+            block = 44
+            postal_code     = [0x50,0x4f,0x53,0x54, 0x43,0x4f,0x44,0x45, 0x3a,0x48,0x33,0x47, 0x31,0x4d,0x38,0x00]
+            if write_block(connection, sector, block, postal_code):
+                print("Data written successfully!")
+
+            # PINCODE:XXXX
+            block = 45
+            random.seed(time.time_ns())
+            pin_code = ""
+            for i in range(4):
+                pin_code += str(random.randrange(10))
+            print("Pin Code: ", pin_code)
+            pin_code_data   = [0x50,0x49,0x4e,0x43, 0x4f,0x44,0x45,0x3a] + list(bytearray(pin_code, 'utf-8')) + [0x00,0x00,0x00,0x00]
+            print("Pin data: ", pin_code_data)
+            if write_block(connection, sector, block, pin_code_data):
+                print("Data written successfully!")
+
+            # BIRTH:30/06/1998
+            block = 46
+            birthdate     = [0x42,0x49,0x52,0x54,0x48,0x3a,0x33,0x30,0x2f,0x30,0x36,0x2f,0x31,0x39,0x39,0x38]
+            if write_block(connection, sector, block, birthdate):
+                print("Data written successfully!")
+            
+            connection.disconnect()
+            
+            print("Remove card!")
+            time.sleep(1.75)
+
+        except Exception as e:
+            pass
 
 if __name__ == "__main__":
     main()
